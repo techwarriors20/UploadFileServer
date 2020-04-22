91f6bacb169110dd758d31407c9b77440053c30e
techwarriors20 <techwarriors20@gmail.com>
2020-04-22 19:55:35 +0530

    Upoad to azure blob in storage added

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -6,8 +6,10 @@ using Azure.Storage;
 using Azure.Storage.Files.Shares;
 using Azure.Storage.Files.Shares.Models;
 using Azure;
+using Azure.Storage.Blobs;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
+using Azure.Storage.Blobs.Models;
 
 namespace UploadFilesServer.Controllers
 {
@@ -72,42 +74,15 @@ namespace UploadFilesServer.Controllers
             try
             {
                 var file = Request.Form.Files[0];
-                var folderName = Path.Combine("StaticFiles", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-
+                          
                 if (file.Length > 0)
                 {
                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
-
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
-                    {
-                        file.CopyTo(stream);
-                    }
-                    
-                    // Get a reference to a share and then create it
-                    ShareClient share = new ShareClient("DefaultEndpointsProtocol=https;AccountName=faceidentity;AccountKey=N/gO9IA0exmgv4yH+ZISlxc0wxv2haDMWi2fZoL2/ErfqM/KHAePM4qkwMjnWEXesDvDidDVYJqKAtv3FvaXnQ==;EndpointSuffix=core.windows.net", "employeesimages");
-                   // share.Create();
-
-                    // Get a reference to a directory and create it
-                    ShareDirectoryClient directory = share.GetDirectoryClient("auth");
-                   // directory.Create();
-
-                    // Get a reference to a file and upload it
-                    ShareFileClient fileAzure = directory.GetFileClient(fileName);
-                    using (FileStream stream = System.IO.File.OpenRead(fullPath))
-                    {
-                        fileAzure.Create(stream.Length);
-                        fileAzure.UploadRange(
-                            new HttpRange(0, stream.Length),
-                            stream);
-                    }
-
-                    string fileAzurePath = fileAzure.Uri.ToString();
-                    //System.IO.File.Delete(fullPath);
-
-                    return Ok(new { fileAzurePath });
+                    BlobClient blobShare = new BlobClient("DefaultEndpointsProtocol=https;AccountName=imagefaceprocess;AccountKey=JMNqY9DyKRnPZHSd6r7eUnBzoxHDsHWrweE8eG9z/x1E/NqoSihrv5VgmJJbNIjapylEvsqFcKDXnUkv4lEmnQ==;EndpointSuffix=core.windows.net", "imagesprocess", fileName);
+                    string imageUri = blobShare.Uri.AbsoluteUri;
+                    var contentInfo = blobShare.Upload(file.OpenReadStream());
+                  
+                    return Ok(new { imageUri });
                 }
                 else
                 {
